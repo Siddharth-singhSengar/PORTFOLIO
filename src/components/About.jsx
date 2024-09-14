a7a703aba7ebcf1f7296681300f0717e068eeb04
Siddharth-singhSengar <156781416+Siddharth-singhSengar@users.noreply.github.com>
2024-09-14 18:17:30 +0530

    About page

---
+++ src/components/About.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Siddharth, a dedicated web developer with a strong focus on
+          the MERN Stack. Leveraging my background in IT, I am committed to
+          delivering innovative and visually compelling software solutions that
+          make a lasting impact. My passion lies in crafting user-centric
+          applications that blend functionality with exceptional design.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          I hold a Bachelor of Technology (B.Tech) degree in Computer Science
+          from RGPV (2018-2022), where I developed a strong foundation in
+          software development and modern web technologies. My education during
+          this period equipped me with the skills necessary to tackle complex
+          problems and build efficient, scalable solutions.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          I am proficient in Python and JavaScript, with experience in
+          frameworks and technologies such as React.js, Node.js, Django, and
+          MongoDB. I have a strong grasp of web development, design principles,
+          and scalable architectures. My excellent problem-solving skills allow
+          me to approach challenges methodically, while my ability to
+          communicate and collaborate effectively ensures successful project
+          outcomes within cross-functional teams.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          I am currently working as an Associate Software Engineer (ASE) at
+          Accenture, where I contribute to the development and deployment of
+          software solutions. In this role, I collaborate with cross-functional
+          teams to deliver high-quality, scalable applications, while adhering
+          to industry best practices. My work involves leveraging my expertise
+          in programming, frameworks, and design principles to solve complex
+          problems and meet client needs efficiently.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>Python Certification , Hackathons</span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative solutions that exceed client expectations and positively
+          impact the digital landscape. I am committed to continuous learning
+          and always seek new challenges to grow and expand my expertise.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
