9cad9fb68679da99eb7433b845a456959208c0b1
Siddharth-singhSengar <156781416+Siddharth-singhSengar@users.noreply.github.com>
2024-09-16 19:15:57 +0530

    Portfolio and Experience page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import About from "./components/About"
+import Experiance from "./components/Experience"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import PortFolio from "./components/Portfolio"
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
      <Navbar/>
      <Home/>
      <About/>
+     <PortFolio/>
+     <Experiance/>
     </>
   
 
